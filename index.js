ff77021f9e0fe6b859b0e5d59208188f0094ab3a
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-11 02:52:52 +0000

    splits up the server.js file

---
+++ index.js
@@ -0,0 +1,38 @@
+/*
+* server.js
+*
+* Copyright (C) 2024 Aniruddhsinh Jadeja - All Rights  Reserved
+* You may use and modify the code to support the needs of Mplus
+* Application. You  may  add your name  as the author under the 
+* original author name. 
+*
+* Under  no  circumstances  the  code  should be distributed to 
+* anyone who is not  a part  of Mplus  application  development 
+* team.
+* 
+* @originalAuthor Aniruddhsinh Jadeja
+* Version 1.0.0.0
+* @application MPlus
+*/
+
+require('dotenv').config();
+const { PORT, app } = require ("./server")
+
+const req = {
+    get: function(headerName) {
+      // return the value of the specified header
+      console.log("headerName :",headerName)
+      return `local${headerName}:10000`;
+    },
+    protocol: "http"
+  };
+
+
+app.listen(PORT, () => {
+
+    // print the server url
+    const hostname = req.get("host");
+    const protocol = req.protocol;
+    console.clear();
+    console.log(`Server is running on ${protocol}://${hostname}`);
+  });
