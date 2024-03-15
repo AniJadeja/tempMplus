696562afdb37b1ddc87df42cef1744317d293208
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-15 20:07:55 +0000

    Adds the comments in the files

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,23 @@
-const {pingEP : pingEndPoint } = require("./endpoints")
+/*
+* index.js
+*
+* Copyright (C) 2024 Aniruddhsinh Jadeja - All Rights  Reserved
+* You may use and modify the code to support the needs of Mplus
+* Application. You  may  add your name  as the author under the 
+* original author name. 
+*
+* Under  no  circumstances  the  code  should be distributed to 
+* anyone who is not  a part  of Mplus  application  development 
+* team.
+* 
+* @originalAuthor Aniruddhsinh Jadeja
+* Version 1.0.0.0
+* @application MPlus
+*/
+
+
+// Importing the pingEndPoint from the endpoints.js file
+const { pingEP: pingEndPoint } = require("./endpoints")
 
 const PORT = process.env.PORT;
 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,21 @@
+/*
+* index.js
+*
+* Copyright (C) 2024 Aniruddhsinh Jadeja - All Rights  Reserved
+* You may use and modify the code to support the needs of Mplus
+* Application. You  may  add your name  as the author under the 
+* original author name. 
+*
+* Under  no  circumstances  the  code  should be distributed to 
+* anyone who is not  a part  of Mplus  application  development 
+* team.
+* 
+* @originalAuthor Aniruddhsinh Jadeja
+* Version 1.0.0.0
+* @application MPlus
+*/
+
+
 const { PORT } = require("./config")
 const { app  } = require("./app")
 
