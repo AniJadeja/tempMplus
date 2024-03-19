5a48dbf726cd45ab02eb004225a26449a502e2e8
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-19 04:16:20 +0000

    Adds default routes

---
+++ server/modules/app/routes.js
@@ -0,0 +1,27 @@
+/*
+* server/modules/app/routes.js
+*
+* Copyright (C) 2024 Anirudhdhsinh Jadeja - All Rights  Reserved
+* You may use and modify the code to support the needs of Mplus
+* Application. You  may  add your name  as the author under the
+* original author name.
+*
+* Under  no  circumstances  the  code  should be distributed to
+* anyone who is not  a part  of Mplus  application  development
+* team.
+*
+* @originalAuthor Anirudhdhsinh Jadeja
+* Version 1.0.0.0
+* @application mplus-admin-server
+*/
+
+// Libraries
+const { app } = require("./middlewares");
+const { pingEndPoint } = require ("@config");
+
+app.use(pingEndPoint, (req,res)=>{
+    res.status(200).json({message:"Mplus pinged.."})
+});
+
+
+module.exports = { app } 
