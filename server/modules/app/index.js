784483f6b4fd3568dc1055d228bfbdc3c876c7f3
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-19 04:15:23 +0000

    Adds an entryfile

---
+++ server/modules/app/index.js
@@ -0,0 +1,23 @@
+/*
+* server/modules/app/index.js
+*
+* Copyright (C) 2024 Anirudhdhsinh Jadeja - All Rights  Reserved
+* You may use and modify the code to support the needs of Mplus
+* Application. You  may  add your name  as the author under the
+* original author name.
+*
+* Under  no  circumstances  the  code  should be distributed to
+* anyone who is not  a part  of Mplus  application  development
+* team.
+*
+* @originalAuthor Anirudhdhsinh Jadeja
+* Version 1.0.0.0
+* @application mplus-admin-server
+*/
+
+// Libraries
+const { app } = require('./routes');
+
+module.exports = {
+    app
+}
