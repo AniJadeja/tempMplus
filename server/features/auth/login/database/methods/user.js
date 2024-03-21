905c957ee3bc9ff5cd0a22949c407a5a67b56c5c
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-21 04:52:30 +0000

    Detaches the JWT module

--- server/features/auth/login/database/methods/user.js
+++ server/features/auth/login/database/methods/user.js
@@ -17,40 +17,50 @@
 
 const { user } = require("../model");
 
+const services = require("@services");
+
 const login = async (userEmail, password) => {
-    try {
-        // Check if the email exists
-        if (!user.email === userEmail) {
-        throw new Error("Invalid email");
-        }
-    
-        // Check if the password is correct
-        const isMatch = password === user.password;
-        if (!isMatch) {
-        throw new Error("Invalid password");
-        }
-    
-        // Generate JWT token
-        const token = jwt.sign(
-        {
-            email: user.email,
-        },
-        JWT_SECRET
-        );
-    
-        const response = {
-        success : "true",
-        message : "Login successful",
-        data : {
-            token : token
-        },
-        };
-        return response;
-    } catch (error) {
-        return { error: error.message };
+  try {
+    // Check if the email exists
+    if (!user.email === userEmail) {
+      throw new Error("Invalid email");
     }
+
+    // Check if the password is correct
+    const isMatch = password === user.password;
+    if (!isMatch) {
+      throw new Error("Invalid password");
+    }
+
+    // Generate JWT token
+    const token = await services.signJWT(user.email);
+    if (!token) {
+      throw new Error("Token generation failed");
+    }
+    // Unix timestamp value
+    const unixTimestamp = 1710989257;
+
+    // Convert Unix timestamp to milliseconds
+    const milliseconds = unixTimestamp * 1000;
+
+    // Create a new Date object using the milliseconds
+    const date = new Date(milliseconds);
+
+    // Output the date and time in a readable format
+    console.log(date.toLocaleString());
+    const response = {
+      success: "true",
+      message: "Login successful",
+      data: {
+        token: token,
+      },
     };
+    return response;
+  } catch (error) {
+    return { error: error.message };
+  }
+};
 
 module.exports = {
-    login
-    };
+  login,
+};
