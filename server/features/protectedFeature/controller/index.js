2531e4ce6ce23e58c5586ccb4400e0b1b611fe1c
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-21 05:01:58 +0000

    Adds a new protected feature

---
+++ server/features/protectedFeature/controller/index.js
@@ -0,0 +1,22 @@
+/*
+ * ./server/features/protectedFeature/controller/index.js
+ *
+ * Copyright (C) 2024 Aniruddhsinh Jadeja - All Rights  Reserved
+ * You may use and modify the code to support the needs of Mplus
+ * Application. You  may  add your name  as the author under the
+ * original author name.
+ *
+ * Under  no  circumstances  the  code  should be distributed to
+ * anyone who is not  a part  of Mplus  application  development
+ * team.
+ *
+ * @originalAuthor Aniruddhsinh Jadeja
+ * Version 1.0.0.0
+ * @application MPlus
+ */
+
+
+exports.protected = async (req, res) => {
+  res.status(200);
+  res.json({ message: "Successfully Authenticated.." });
+};
---
+++ server/features/protectedFeature/route/index.js
@@ -0,0 +1,27 @@
+/*
+* /server/features/login/Route/index.js
+*
+* Copyright (C) 2024 Aniruddhsinh Jadeja - All Rights  Reserved
+* You may use and modify the code to support the needs of Mplus
+* Application. You  may  add your name  as the author under the 
+* original author name. 
+*
+* Under  no  circumstances  the  code  should be distributed to 
+* anyone who is not  a part  of Mplus  application  development 
+* team.
+* 
+* @originalAuthor Aniruddhsinh Jadeja
+* Version 1.0.0.0
+* @application MPlus
+*/
+
+// Libraries
+const express = require('express');
+const { protected } = require('../controller');
+const protectedRouter = express.Router();
+const { verifyToken } = require('../middlewares');
+
+
+protectedRouter.post("/", verifyToken, (req, res) => protected(req, res));
+
+module.exports = { protectedRouter };
