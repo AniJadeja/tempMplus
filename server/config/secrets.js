94f2f131a629334dfffac590476ae6213a63ec28
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-05-12 14:37:33 +0000

    Adds configuration file

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -5,7 +5,10 @@
 // JWT Secret for jwt token signing and verification
 const JWT_SECRET = process.env.JWT_SECRET
 
+// DATABASE_URI for the database connection
+const DATABASE_URI = process.env.DATABASE_URI
+
 /*
 * Export the secrets
 */
-module.exports = { JWT_SECRET } 
+module.exports = { JWT_SECRET , DATABASE_URI} 
