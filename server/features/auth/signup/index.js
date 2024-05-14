7d4cc1fd065bf5183f7b81298a8cbbf46451ac75
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-05-14 13:13:33 +0000

    exports signup router

---
+++ server/features/auth/signup/index.js
@@ -0,0 +1,24 @@
+/*
+* server/features/auth/signup/index.js
+*
+* Copyright (C) 2024 Anirudhdhsinh Jadeja - All Rights  Reserved
+* You may use and modify the code to support the needs of Mplus
+* Application. You  may  add your name  as the author under the
+* original author name.
+*
+* Under  no  circumstances  the  code  should be distributed to
+* anyone who is not  a part  of Mplus  application  development
+* team.
+*
+* @originalAuthor Anirudhdhsinh Jadeja
+* Version 1.0.0.0
+* @application mplus-admin-server
+*/
+
+// Libraries
+const { signupRouter } = require('./Route');
+
+/*
+* Export the router
+*/
+module.exports = { signupRouter };
