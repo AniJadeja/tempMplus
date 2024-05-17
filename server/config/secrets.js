82d3d1ad1fd290e289da245e4b53daf785367d0d
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-05-17 16:19:27 +0000

    Adds the forms and users database

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -8,7 +8,13 @@ const jwt = process.env.JWT_SECRET
 // DATABASE_URI for the database connection
 const db_uri = process.env.DATABASE_URI
 
+// DATABASE_URI for the Users database connection
+const db_uri_users = process.env.USERS_DATABASE_URI
+
+// DATABASE_URI for the Forms database connection
+const db_uri_forms = process.env.FORMS_DATABASE_URI
+
 /*
 * Export the secrets
 */
-module.exports = { jwt, db_uri} 
+module.exports = { jwt, db_uri, db_uri_users, db_uri_forms} 
