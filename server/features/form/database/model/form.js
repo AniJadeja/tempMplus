8bfdb7798bdb15406b135c4284347a62b3e025b6
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-05-19 23:09:17 +0000

    Adds forms db model

---
+++ server/features/form/database/model/form.js
@@ -0,0 +1,50 @@
+/*
+ * ./server/features/form/database/model/form.js
+ *
+ * Copyright (C) 2024 Aniruddhsinh Jadeja - All Rights  Reserved
+ * You may use and modify the code to support the needs of Mplus
+ * Application. You  may  add your name  as the author under the
+ * original author name.
+ *
+ * Under  no  circumstances  the  code  should be distributed to
+ * anyone who is not  a part  of Mplus  application  development
+ * team.
+ *
+ * @originalAuthor Aniruddhsinh Jadeja
+ * Version 1.0.0.0
+ * @application MPlus
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// User Libraries
+const { getConnection } = require("@database");
+
+const { FORMS_DATABASE } = require("@config");
+
+// User Schema
+const formSchema = new Schema({
+    schemaVersion: { type: String, required: true },
+    formId: { type: String, required: true },
+    formName: { type: String, required: true },
+    formDescription: { type: String, required: true },
+    formType: { type: String, required: true },
+    formStatus: { type: String, required: true },
+    formVersion: { type: String, required: true },
+    formFields: { type: Array, required: true },
+    createdBy: { type: String, default: "system" },
+    updatedBy: { type: String, default: "system" },
+    metadata:{type : Object, default: {}}
+});
+
+
+module.exports = () => {
+    // connection
+    const FormssConnection = getConnection(FORMS_DATABASE);
+
+    // User Model
+    const Form = FormssConnection.model('Form', formSchema);
+
+    return Form;
+};
